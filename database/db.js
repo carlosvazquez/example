53ba065dfc173a937c1740efeee9c6a20a520a10
Carlos Vazquez <carlosvazquez@users.noreply.github.com>
2022-10-10 23:30:35 -0500

    Update db.js
    Add db config

--- database/db.js
+++ database/db.js
@@ -1,9 +1,16 @@
+const DATABASE = process.env.DATABASE;
+const DB_HOST = process.env.DB_HOST;
+const DB_PORT = process.env.DB_PORT;
+const PASSWORD = process.env.PASSWORD;
+const USER = process.env.USER;
+
 const mysql2 = require('mysql2');
+
 const conexion = mysql2.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'n1H0nJ!n',
-    database: 'crude_node_db'
+    host: DB_HOST,
+    user: USER,
+    password: PASSWORD,
+    database: DATABASE
 });
 
 conexion.connect((error)=>{
