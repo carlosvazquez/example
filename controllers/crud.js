511eb737167862594deb7a95b9964066ae45b0dc
ADVMCA <cvazquezm2002@alumno.ipn.mx>
2022-10-10 22:54:39 -0500

    First release of Hello World!

--- controllers/crud.js
+++ controllers/crud.js
@@ -3,7 +3,8 @@ const conexion = require('../database/db');
 exports.save = (req,res)=>{
     const user = req.body.user;
     const rol = req.body.rol;
-    conexion.query('INSERT INTO users SET ?',{user:user, rol:rol}, (error, results)=>{
+    const cap = req.body.cap;
+    conexion.query('INSERT INTO users SET ?',{user:user, rol:rol, cap:cap}, (error, results)=>{
         if(error){
             console.log(error);
         }else{
@@ -18,7 +19,8 @@ exports.update = (req, res)=>{
     const id = req.body.id;
     const user = req.body.user;
     const rol = req.body.rol;
-    conexion.query('UPDATE users SET ? WHERE id=?', [{user:user, rol:rol}, id], (error,results)=>{
+    const cap = req.body.cap;
+    conexion.query('UPDATE users SET ? WHERE id=?', [{user:user, rol:rol, cap:cap}, id], (error,results)=>{
         if(error){
             console.log(error);
         }else{
